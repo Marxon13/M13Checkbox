361c0f0e327c41d7ceea7b2f371050d0ad132e4a
Brandon McQuilkin <bmcquilkin@cardinalsolutions.com>
2016-04-13 13:29:19 -0400

    Added Swift Package Manager Support
    I think… can’t install Swift 3 until it is out of beta.

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  M13Checkbox
+//
+//  Created by McQuilkin, Brandon on 4/13/16.
+//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "M13Checkbox"
+)
