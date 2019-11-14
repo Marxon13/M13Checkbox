638a965963986832cbc336c9042d4a465cc5fdf0
Lukas Gergel <lukas.gergel@zonky.cz>
2019-11-14 16:28:45 +0100

    Swift Package Manager support

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.0
 //
 //  Package.swift
 //  M13Checkbox
@@ -9,5 +10,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "M13Checkbox"
+    name: "M13Checkbox",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(name: "M13Checkbox", targets: ["M13Checkbox"]),
+    ],
+    targets: [
+        .target(name: "M13Checkbox", path: "Sources"),
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
 )
