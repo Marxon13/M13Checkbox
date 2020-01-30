cf0a46bf0be920249b1827bc73af0caf207eda7a
Linus <linus@geffarth.com>
2020-01-30 13:04:36 +0100

    updated Package.swift for newest SPM support

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
-//
-//  Package.swift
-//  M13Checkbox
-//
-//  Created by McQuilkin, Brandon on 4/13/16.
-//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
-//
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "M13Checkbox"
+    products: [
+        .library(name: "M13Checkbox", targets: ["M13Checkbox"])
+    ],
+    targets: [
+        .target(
+            name: "M13Checkbox",
+            path: "Sources"
+        )
+    ]
 )
